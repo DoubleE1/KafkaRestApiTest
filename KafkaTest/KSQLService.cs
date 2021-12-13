abd06b09dd2725af0b968f582c1c3f06d2aaf346
DoubleE1 <40216163+DoubleE1@users.noreply.github.com>
2021-12-13 17:33:19 +0800

    Init Project

---
+++ KafkaTest/KSQLService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaTest
+{
+    public class KSQLService
+    {
+        private List<Task> runningTask;
+        public void OnStart()
+        {
+            try
+            {
+                KsqlRestHelper helper = new KsqlRestHelper();
+                runningTask = new List<Task>();
+
+                KsqlRestHelper ksqlRestHelper = new KsqlRestHelper();
+
+                //runningTask.Add(Task.Factory.StartNew(() => ksqlRestHelper.SubscribeAsync(), TaskCreationOptions.LongRunning));
+                runningTask.Add(Task.Factory.StartNew(() => ksqlRestHelper.PushQueryAsync("SELECT * FROM MyClasses EMIT CHANGES;", KsqlRestHelper.OffSetType.Earliest), TaskCreationOptions.LongRunning));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void OnStop()
+        {
+            Console.WriteLine("Service stopped.");
+        }
+    }
+}
---
+++ KafkaTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using ksqlDB.RestApi.Client.KSql.Linq;
+using ksqlDB.RestApi.Client.KSql.Query.Context;
+using ksqlDB.RestApi.Client.KSql.Query.Operators;
+using ksqlDB.RestApi.Client.KSql.RestApi;
+using ksqlDB.RestApi.Client.KSql.RestApi.Http;
+using ksqlDB.RestApi.Client.KSql.RestApi.Serialization;
+using ksqlDB.RestApi.Client.KSql.RestApi.Statements;
+using Nito.AsyncEx;
+using System;
+using Topshelf;
+
+namespace KafkaTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HostFactory.Run(x =>
+                {
+                    x.Service<KSQLService>(s =>
+                    {
+                        s.ConstructUsing(name => new KSQLService());
+                        s.WhenStarted(tc => tc.OnStart());
+                        s.WhenStopped(tc => tc.OnStop());
+                    });
+                    x.RunAsLocalSystem();
+                    x.SetDescription("KafkaTest");
+                    x.SetDisplayName("KafkaTest");
+                    x.SetServiceName("KafkaTest");
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        
+    }
+}
